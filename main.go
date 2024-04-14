47e7d58e266dd4d10002e88436f15c05d4ec8edf
Shubham Katara <shubham.katara@trivago.com>
2024-04-14 19:35:10 +0200

    Fetch one event

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"example.com/api/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	server := gin.Default()
 	server.GET("/", utils.GetRoot)
-	server.GET("/event/listall", utils.GetAllEvents)
-	server.POST("/event/add", utils.SaveEvent)
+	server.GET("/event/all/", utils.GetAllEvents)
+	server.POST("/event/add/", utils.AddEvent)
+	server.POST("/event/fetch/", utils.FetchEvent)
 	server.Run(":8080")
 
 }
--- utils/api_routes.go
+++ utils/api_routes.go
@@ -7,6 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func checkError(e error) {
+	if e != nil {
+		fmt.Println("Error: ", e)
+	}
+}
+
 type Event struct {
 	EventName  string `json:"event_name" binding:"required"`
 	EventOwner string `json:"event_owner" binding:"required"`
@@ -24,19 +30,30 @@ func GetAllEvents(c *gin.Context) {
 	})
 }
 
-func SaveEvent(c *gin.Context) {
+func AddEvent(c *gin.Context) {
 	event_id := rand.Intn(100000)
 	var jsonData Event
 	err := c.ShouldBindJSON(&jsonData)
-	if err != nil {
-		fmt.Println("Error in binding JSON: ", err)
-	}
+	checkError(err)
 	events = append(events, Event{
 		EventName:  jsonData.EventName,
 		EventOwner: jsonData.EventOwner,
 		EventID:    event_id,
 	})
 
+}
+
+func FetchEvent(c *gin.Context) {
+	var jsonData Event
+	err := c.ShouldBindJSON(&jsonData)
+	checkError(err)
+	for _, event := range events {
+		if event.EventID == jsonData.EventID {
+			c.JSON(200, gin.H{
+				"event": event,
+			})
+		}
+	}
 }
 
 func GetRoot(c *gin.Context) {
